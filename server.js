6522faa188dfdc8bd52a7dce27919153113c0419
tdreid <trevor.d.reid@gmail.com>
2017-05-09 21:03:03 +0000

    Serve static assets; pad at top

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
 
+app.use(express.static('static'));
+
 app.get('/', function(req,res){
     res.render('index');
 });
